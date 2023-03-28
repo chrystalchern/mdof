a822213eb2a5f05069525af1ce8b8eb29006cc73
Chrystal Chern <52893467+chrystalchern@users.noreply.github.com>
2023-03-28 11:48:35 -0700

    cc - ssid package

--- src/ssid/realize.py
+++ src/ssid/realize.py
@@ -43,3 +43,60 @@ def era(Y,no,nc,r=None):
     Cr = (Ur @ SigmaSqrt)[:p,:]
 
     return (Ar,Br,Cr,Dr)
+
+
+# l = initial lag for data correlations
+# g = lags (gap) between correlation matrices
+# a = (alpha) number of block rows in Hankel of correlation matrix
+# b = (beta) number of block columns in Hankel of correlation matrix
+def era_dc(Y,no,nc,a,b,l,g,r=None):
+    if not r:
+        r = int(Y.shape[2]/2)
+    p,q = Y.shape[:2] # p = number of outputs, q = number of inputs
+
+    # get D from first p x q block of impulse response
+    Dr = Y[:,:,0]  # first block of output data
+    
+    assert Y.shape[2] >= l+(a+1+b+1)*g+no+nc   # make sure there are enough timesteps to assemble the Hankel matrices
+    
+    # Hankel matrix of impulse response (Markov parameters)
+    H = np.zeros((p*(no), q*(nc+l+(a+1+b+1)*g)))
+    for i in range(no):
+        for j in range(nc+l+(a+1+b+1)*g):
+            H[p*i:p*(i+1), q*j:q*(j+1)] = Y[:,:,i+j+1]
+    H0 = H[:,:q*nc]
+    assert H0.shape == (p*(no), q*(nc))
+
+    dimR = p*no # Dimension of square correlation matrices
+    dimHRl = (dimR*(a+1), dimR*(b+1)) # Dimension of Hankel matrix of correlation matrices
+    HRl = np.zeros(dimHRl) # Hankel matrix of correlation matrices
+    HRl1 = np.zeros(dimHRl) # Shifted Hankel matrix of correlation matrices
+    for i in range(a+1):
+        for j in range(b+1):
+            Hl = H[:, q*(l+1+(i+j)*g):q*(l+1+(i+j)*g+nc)]
+            Hl1 = H[:, q*(l+1+(i+j)*g+1):q*(l+1+(i+j)*g+nc+1)]
+            assert Hl.shape == Hl1.shape == (p*(no), q*(nc))
+            R = Hl@H0       # correlation matrix
+            R1 = Hl1@H0     # shifted correlation matrix
+            assert R.shape == R1.shape == (dimR,dimR)
+            HRl[dimR*i:dimR*(i+1), dimR*j:dimR*(j+1)] = R
+            HRl1[dimR*i:dimR*(i+1), dimR*j:dimR*(j+1)] = R1
+
+    # reduced SVD of Hankel matrix of correlation matrices
+    def _svd(*args):
+        U,S,V = scipy.linalg.svd(*args, lapack_driver="gesvd")
+        return U,S,V.T.conj()
+    U,S,V = _svd(HRl)
+    SigmaInvSqrt = np.diag(S[:r]**-0.5)
+    SigmaSqrt = np.diag(S[:r]**0.5)
+    Ur = U[:,:r]
+    Vr = V[:,:r]
+
+    # get A from SVD and shifted Hankel matrix of correlation matrices
+    Ar = SigmaInvSqrt @ Ur.T.conj() @ HRl1 @ Vr @ SigmaInvSqrt
+
+    # get B and C
+    Br = ((SigmaInvSqrt @ Ur.T.conj())[:,:dimR] @ H0)[:,:q]
+    Cr = (Ur @ SigmaSqrt)[:p,:]
+
+    return (Ar,Br,Cr,Dr)
