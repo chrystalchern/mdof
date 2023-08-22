9e637ca6d58b5514ea644ace231e51e9065e4f86
claudio perez <50180406+claudioperez@users.noreply.github.com>
2023-08-22 12:04:56 -0700

    cmp

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,50 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+#
+from pathlib import Path
 project = 'ssid'
 copyright = '2023, Chrystal Chern'
 author = 'Chrystal Chern'
-release = '0.0.0'
+description = "Fast and friendly structural system identification."
+version = '0.0.4'
+release = '0.0.4'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["nbsphinx"]
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
+    'nbsphinx'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+
+source_suffix = '.rst'
+root_doc = 'index'
+language = 'en'
+
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_title = project
 html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
 html_favicon = '_static/favicon.ico'
 html_css_files = [
     "css/peer.css",
+] + [
+    'css/css/'+str(file.name) for file in (Path(__file__).parents[0]/"_static/css/css/").glob("*.css")
 ]
-html_title = project
+html_additional_pages = {'index': 'home.html'}
+html_context = {
+    'description': description,
+    **globals()
+}
 html_show_sourcelink = False
 html_theme_options = {
     "github_url": f"https://github.com/BRACE2/{project}"
 }
 
+autodoc_member_order = 'bysource'
