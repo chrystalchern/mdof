e679aac9a3104bb6d2125e74d7f9529f0d4dc880
claudio perez <50180406+claudioperez@users.noreply.github.com>
2023-08-25 04:42:45 -0700

    cmp

--- tests/test_srim.sh
+++ tests/test_srim.sh
@@ -4,4 +4,4 @@
 # or
 #      bash test_srim.sh <motion.zip>
 
-python -m ssid srim $1 --ss-size 30 --arx-order 100 --inputs '[3, 6, 17]' --outputs '[12, 14, 19]'
+python -m mdof srim $1 --ss-size 30 --arx-order 100 --inputs '[3, 6, 17]' --outputs '[12, 14, 19]'
